8fe4d2a9d9b628fb569491a62f631b60d583ca78
deeplydrumming <no@no.it>
2024-07-25 21:18:31 +0100

    successfully migrated to pnpm !!! changed all scripts

--- server/src/helpers/server-callbacks.ts
+++ server/src/helpers/server-callbacks.ts
@@ -17,14 +17,14 @@ export function getErrorCb(port: number | string | boolean) {
 
 		// handle specific listen errors with friendly messages
 		switch (error.code) {
-			case 'EACCES': {
+			case 'EACCES':
 				logger.error(bind + ' requires elevated privileges')
 				process.exit(1)
-			}
-			case 'EADDRINUSE': {
+				break
+			case 'EADDRINUSE':
 				logger.error(bind + ' is already in use')
 				process.exit(1)
-			}
+				break
 			default:
 				throw error
 		}
