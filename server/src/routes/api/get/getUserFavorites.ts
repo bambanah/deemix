321713d1992198ca115f8cebe1bda38a2ed57376
Lachie <lachlanu@gmail.com>
2024-08-21 19:25:39 +1000

    Disable deezer favourite tracks fetch

--- server/src/routes/api/get/getUserFavorites.ts
+++ server/src/routes/api/get/getUserFavorites.ts
@@ -17,7 +17,8 @@ const handler: ApiHandler['handler'] = async (req, res) => {
 		result.playlists = await dz.gw.get_user_playlists(userID, { limit: -1 })
 		result.albums = await dz.gw.get_user_albums(userID, { limit: -1 })
 		result.artists = await dz.gw.get_user_artists(userID, { limit: -1 })
-		result.tracks = await dz.gw.get_my_favorite_tracks({ limit: -1 })
+		// TODO: Lazy load favourites when navigating to relevant tab
+		// result.tracks = await dz.gw.get_my_favorite_tracks({ limit: -1 }) // Reenable once this isn't so slow
 		result.lovedTracks = `https://deezer.com/playlist/${dz.current_user.loved_tracks}`
 	} else {
 		result = { error: 'notLoggedIn' }
