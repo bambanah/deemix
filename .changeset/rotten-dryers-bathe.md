3bc231d051f3e1bfcee6cba77e8870b98a0da40b
Lachie Underhill <lachlanu@gmail.com>
2024-08-23 17:16:58 +1000

    chore: break BaseTabs component into two

---
+++ .changeset/rotten-dryers-bathe.md
@@ -0,0 +1,5 @@
+---
+"deemix-webui": patch
+---
+
+Break out BaseTabs component into two
--- webui/src/components/globals/BaseTab.js
+++ webui/src/components/globals/BaseTab.js
@@ -2,14 +2,14 @@ import { defineComponent } from "@vue/composition-api";
 
 // https://vuejs.org/v2/guide/render-function.html
 // https://vuejs.org/v2/guide/render-function.html#createElement-Arguments
-export const BaseTabs = defineComponent({
-	name: "BaseTabs",
+export const BaseTab = defineComponent({
+	name: "BaseTab",
 	functional: true,
 	render(h, ctx) {
 		return h(
-			"ul",
+			"li",
 			{
-				class: [ctx.data.class, "my-8", "section-tabs"],
+				class: [ctx.data.class, "section-tabs__tab", "uppercase-first-letter"],
 				on: ctx.data.on,
 			},
 			ctx.slots().default
