1fcbdb13f1b13c9fe23e3fb476863d14f93d1acd
Lachie Underhill <lachlanu@gmail.com>
2024-09-06 19:51:01 +1000

    chore: simplify package build scripts

--- gui/forge.config.mjs
+++ gui/forge.config.mjs
@@ -21,8 +21,10 @@ export default {
 			config: {},
 		},
 		{
-			name: "@electron-forge/maker-zip",
-			platforms: ["darwin"],
+			name: "@electron-forge/maker-dmg",
+			config: {
+				format: "ULFO",
+			},
 		},
 		{
 			name: "@electron-forge/maker-deb",
