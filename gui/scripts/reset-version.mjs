6a835ccd3bdf14a94ace73f88659c70aedb966fa
Lachie Underhill <lachlanu@gmail.com>
2024-09-06 13:08:25 +1000

    chore: convert gui to typescript

--- gui/scripts/reset-version.mjs
+++
@@ -1,6 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-
-const pack = JSON.parse(readFileSync("package.json"));
-pack.version = "0.0.0";
-
-writeFileSync("package.json", JSON.stringify(pack, null, "\t") + "\n");
