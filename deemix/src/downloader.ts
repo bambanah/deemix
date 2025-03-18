d4015d379f5a1c425e3594a04c1c8d9d6d7f81dd
Lachie <lachlanu@gmail.com>
2025-03-18 20:58:59 +1000

    fix(deemix): title casing not being applied

--- deemix/src/downloader.ts
+++ deemix/src/downloader.ts
@@ -234,6 +234,8 @@ export class Downloader {
 		track.bitrate = selectedFormat;
 		track.album.bitrate = selectedFormat;
 
+		track.applySettings(this.settings);
+
 		const { filename, filepath, artistPath, coverPath, extrasPath } =
 			generatePath(track, this.downloadObject.type, this.settings);
 
@@ -288,12 +290,6 @@ export class Downloader {
 			return returnData;
 		}
 
-		// Apply Settings
-		track.applySettings(this.settings);
-
-		// Generate filename and filepath from metadata
-		// const { filename, filepath, artistPath, coverPath, extrasPath } =
-		// 	generatePath(track, this.downloadObject, this.settings);
 		if (this.downloadObject.isCanceled) throw new DownloadCanceled();
 
 		if (this.settings.overwriteFile === OverwriteOption.KEEP_BOTH) {
