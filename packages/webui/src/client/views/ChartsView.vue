e8461b9ebb0094a6d7185561999116affe62c670
Dreamer269 <80389932+Dreamer269@users.noreply.github.com>
2025-12-14 16:31:00 +0100

    fix(webui): add space between the buttons on the chart page

--- packages/webui/src/client/views/ChartsView.vue
+++ packages/webui/src/client/views/ChartsView.vue
@@ -112,16 +112,19 @@ watch(id, (newId) => {
 		</div>
 
 		<div v-else>
-			<button class="btn btn-primary" @click="onChangeCountry">
-				{{ t("charts.changeCountry") }}
-			</button>
-			<button
-				:data-link="'https://www.deezer.com/playlist/' + id"
-				class="btn btn-primary"
-				@click.stop="addToQueue"
-			>
-				{{ t("charts.download") }}
-			</button>
+			<div class="flex gap-x-1">
+				<button class="btn btn-primary" @click="onChangeCountry">
+					{{ t("charts.changeCountry") }}
+				</button>
+				<button
+					:data-link="'https://www.deezer.com/playlist/' + id"
+					class="btn btn-primary"
+					@click.stop="addToQueue"
+				>
+					{{ t("charts.download") }}
+				</button>
+			</div>
+
 			<table class="table--charts table">
 				<tbody>
 					<tr v-for="(track, pos) in chart" :key="pos" class="track_row">
